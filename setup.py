3c57145bffe1e2b99dc1af4275134ba8d54bfacd
kochelmonster <mreithinger@web.de>
2021-07-15 16:35:40 +0200

    updated conda.recipe.

--- setup.py
+++ setup.py
@@ -151,9 +151,7 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: "
-        "GNU Lesser General Public License v3 or later (LGPLv3+)",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules"],
     cmdclass=cmdclass, ext_modules=ext_modules)
