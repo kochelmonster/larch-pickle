c407b8f18668fb609e1caf835a9e980515c57d97
kochelmonster <mreithinger@web.de>
2022-01-01 22:30:04 +0100

    debug msg.

--- github-build/bld.bat
+++ github-build/bld.bat
@@ -1,5 +1,7 @@
 ::build and copy for pip
 %PYTHON% setup.py bdist_wheel
+
+echo "*****COPY WHEEL TO %RUNNER_WORKSPACE%"
 COPY dist\*.whl %RUNNER_WORKSPACE%
 ::install for conda
 %PYTHON% -m pip install --no-deps --ignore-installed --global-option=build .
