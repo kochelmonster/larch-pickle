6932e704364c4b3f0f90b37d926626dc38d5321d
mreithinger <mreithinger@web.de>
2022-01-01 19:13:36 +0100

    conda recipe for github actions.

---
+++ github-build/build.sh
@@ -0,0 +1,5 @@
+# build and copy for pip
+$PYTHON setup.py bdist_wheel
+cp dist/*.whl $RUNNER_WORKSPACE
+# install for conda
+$PYTHON -m pip install --no-deps --ignore-installed --global-option=build .
