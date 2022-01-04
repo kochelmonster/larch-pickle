07ae9fdf1ade840d1ea2759261d1e3165af4be4f
kochelmonster <mreithinger@web.de>
2022-01-04 16:57:10 +0100

    more macos debugging.

--- github-build/build.sh
+++ github-build/build.sh
@@ -1,7 +1,10 @@
 # build and copy for pip
 $PYTHON setup.py bdist_wheel
+echo "-----------ls-------------"
 ls
+echo "-----------ls dist-------------"
 ls dist
+echo "------runner?-----------"
 echo "runner $RUNNER_WORKSPACE"
 cp dist/*.whl $RUNNER_WORKSPACE
 # install for conda
