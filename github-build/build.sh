23104a8133e46a3e99b309185a427a87a6dddf9f
kochelmonster <mreithinger@web.de>
2022-01-04 16:18:45 +0100

    Merge branch 'master' of https://github.com/kochelmonster/larch-pickle

--- github-build/build.sh
+++ github-build/build.sh
@@ -1,5 +1,8 @@
 # build and copy for pip
 $PYTHON setup.py bdist_wheel
+ls
+ls dist
+echo "runner $RUNNER_WORKSPACE"
 cp dist/*.whl $RUNNER_WORKSPACE
 # install for conda
 $PYTHON -m pip install --no-deps --ignore-installed --global-option=build .
